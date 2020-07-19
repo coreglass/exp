e4488dbcf1cdd4d622c05886a9ec8db32304daaa
coreglass <33614974+coreglass@users.noreply.github.com>
2020-07-19 22:24:51 +0800

    Update 002.py

--- 002.py
+++ 002.py
@@ -4,12 +4,12 @@ z = int(input("请输入第三个数字"))
 
 if x > y :
     if x > z:
-        print("请输入第一个数字%d"%x)
+        print("输入第一个数字是%d"%x)
     else:
-        print("请输入第二个数字%d"%z)
+        print("输入第二个数字是%d"%z)
 elif x < y:
     if y > z:
-        print("请输入第三个数字%d"%y)
+        print("输入第三个数字是%d"%y)
     else:
         print("鏈€澶х殑鏁版槸锛�%d"%z)
        
